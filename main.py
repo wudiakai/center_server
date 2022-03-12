5950491202f54109b0f9bdf7627b91e25c75e9d4
kevin <wudiakai@126.com>
2022-03-12 22:54:03 +0800

    add list

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-from markdownReader import read_markdown
+from markdownReader import read_markdown, read_markdown_list
 
 app = FastAPI()
 
@@ -11,6 +11,7 @@ origins = [
     "http://10.1.29.11:8088",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:8088",
 ]
 
 app.add_middleware(
@@ -32,9 +33,14 @@ class Response(BaseModel):
 async def get_markdown(name: str):
     f: str = read_markdown(name)
     if len(f) == 0:
-        return {"msg": 'err: can not find file', 'code': 400, 'data': ''}
+        return {'data': ''}
     else:
-        return {'msg': 'ok', 'code': 200, 'data': f}
+        return {'data': f}
+
+@app.get("/markdown_list")
+async def get_markdown_list():
+    return read_markdown_list()
+
 
 
 @app.get("/")
@@ -44,5 +50,4 @@ async def root():
 
 if __name__ == '__main__':
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=2022, debug=True)
--- markdownReader.py
+++ markdownReader.py
@@ -1,10 +1,37 @@
+import code
 import os.path
 
 
 def read_markdown(name: str):
     file = "markdown/" + name + '.md'
     if os.path.exists(file):
-        f = open(file)
+        f = open(file, encoding='utf-8')
         return f.read()
+    else:
+        return ""
+
+def read_markdown_list():
+    file = 'markdown/mdlist.txt'
+    if os.path.exists(file):
+        lst = []
+        sublst=  []
+        for line in open(file, encoding='utf-8'):
+            # line = f.readline()
+            line = line.replace('\n','')
+            if (line.startswith('--')):
+                sublst.append(line.replace('-',''))
+            else:
+                if len(sublst) > 0:
+                    lst.append(sublst)
+                    sublst=[]
+                lst.append(line)    
+
+        if len(sublst) > 0:
+                    lst.append(sublst)
+                    sublst=[]
+
+        print(lst)
+        return lst        
+
     else:
         return ""
